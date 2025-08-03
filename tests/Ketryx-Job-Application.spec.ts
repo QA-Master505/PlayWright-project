0cf3825f223046b4faed922437ceb7e6fc4b2c4d
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-08-03 15:17:14 +0200

    initial commit

---
+++ tests/Ketryx-Job-Application.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('Validate required fields on job application form', async ({ page }) => {
+  await page.goto('https://www.ketryx.com/');
+  await page.getByTestId('uc-accept-all-button').click();
+  await page.getByRole('link', { name: 'Careers' }).click();
+  await page.getByRole('link', { name: 'Join us' }).click();
+
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'Software Quality Engineer' }).click();
+  const page1 = await page1Promise;
+
+  await page1.waitForLoadState('domcontentloaded');
+
+  // Ensure the Submit button is visible and clickable
+  const submitBtn = page1.getByRole('button', { name: 'Submit application' });
+  await submitBtn.scrollIntoViewIfNeeded(); // Helps with Firefox & WebKit
+  await submitBtn.waitFor({ state: 'visible' });
+  await submitBtn.click();
+
+  // Wait for validation errors to appear
+  await expect(page1.getByTestId('first_name-error')).toHaveText(/First Name is required/i);
+});
---
+++ tests/Record-Test.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('GitHub login test', async ({ page }) => {
+  await page.goto('https://github.com/');
+
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).fill('testertest');
+  await page.getByRole('textbox', { name: 'Username or email address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('test1234');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+});
